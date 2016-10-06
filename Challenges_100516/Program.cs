ebbde8f52b5e872531bf4a9887dae7595f448a64
Shadi Jam <shadijam00@gmail.com>
2016-10-05 22:00:11 -0500

    challenge 2 complete

--- Challenges_100516/Program.cs
+++ Challenges_100516/Program.cs
@@ -3,20 +3,51 @@ using System.Diagnostics;
 
 //public class VectorMagnitute
 // {
-   // static float vectorLength(float[,] vecs)
-   // {
-   //     double dx = (vecs[0, 0] - vecs[1, 0]);
-   //     double dy = (vecs[0, 1] - vecs[1, 1]);
-   //     double Sumsrt = (Math.Sqrt((Math.Pow(dx, 2)) + (Math.Pow(dy, 2))));
+// static float vectorLength(float[,] vecs)
+// {
+//     double dx = (vecs[0, 0] - vecs[1, 0]);
+//     double dy = (vecs[0, 1] - vecs[1, 1]);
+//     double Sumsrt = (Math.Sqrt((Math.Pow(dx, 2)) + (Math.Pow(dy, 2))));
 
-   //     return (float)(Sumsrt);
-   // }
-   // public static void Main()
-   // {
-   //     Console.WriteLine(vectorLength(new float[2, 2] { { 0, 3 }, { 4, 0 } }) == 5);
-   //     Console.WriteLine(vectorLength(new float[2, 2] { { 1, -1 }, { 1, -1 } }) == 0);
-   //     Console.WriteLine(vectorLength(new float[2, 2] { { 0, 1 }, { 0, 0 } }) == 1);
-   // } 
+//     return (float)(Sumsrt);
+// }
+// public static void Main()
+// {
+//     Console.WriteLine(vectorLength(new float[2, 2] { { 0, 3 }, { 4, 0 } }) == 5);
+//     Console.WriteLine(vectorLength(new float[2, 2] { { 1, -1 }, { 1, -1 } }) == 0);
+//     Console.WriteLine(vectorLength(new float[2, 2] { { 0, 1 }, { 0, 0 } }) == 1);
+// } 
 //}
 
 
+
+//using System;
+//using System.Diagnostics;
+
+//public class Function1Part1
+//{
+//    static int sum(int a, int b)
+//    {
+//        return a + b;
+//    }
+//    public static void Main()
+//    {
+//        Console.WriteLine(sum(1, 2) == 3);
+//    }
+//}
+
+
+
+//using System;
+//using System.Diagnostics;
+
+//public class Function1Part2
+//{
+//    int a = 1;
+//    int b = 2;
+//    public static void Main()
+//    {
+//        Func<int, int, int> sum = (a, b) => a + b;
+//        Console.WriteLine(1 + 2);
+//    }
+//}
